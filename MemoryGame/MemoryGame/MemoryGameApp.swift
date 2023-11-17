870688354157d0caf0458fabd47a1277141db908
Ronald Maciel <ronaldsmaciel2@outlook.com>
2023-11-17 15:15:18 -0300

    first commit

---
+++ MemoryGame/MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Ronald on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
